7b28fda54aabf99518fa359f88b3083ba9834db2
platoapp <92391267+platoapp@users.noreply.github.com>
2020-05-20 23:02:33 +0000

    Update dependabot.yml

--- vendor/github.com/emicklei/go-restful/logger.go
+++ vendor/github.com/emicklei/go-restful/logger.go
@@ -14,4 +14,19 @@ func init() {
 	traceLogger = log.Logger // use the package logger by default
 }
 
-// TraceLogger enables detailed logging of H
+// TraceLogger enables detailed logging of Http request matching and filter invocation. Default no logger is set.
+// You may call EnableTracing() directly to enable trace logging to the package-wide logger.
+func TraceLogger(logger log.StdLogger) {
+	traceLogger = logger
+	EnableTracing(logger != nil)
+}
+
+// SetLogger exposes the setter for the global logger on the top-level package
+func SetLogger(customLogger log.StdLogger) {
+	log.SetLogger(customLogger)
+}
+
+// EnableTracing can be used to Trace logging on and off.
+func EnableTracing(enabled bool) {
+	trace = enabled
+}
---
+++ vendor/github.com/emicklei/go-restful/parameter.go
@@ -0,0 +1,11 @@
+package restful
+
+// Copyright 2013 Ernest Micklei. All rights reserved.
+// Use of this source code is governed by a license
+// that can be found in the LICENSE file.
+
+const (
+	// PathParameterKind = indicator of Request parameter type "path"
+	PathParameterKind = iota
+
+	// QueryParameterKind = indicator of Request paramete
