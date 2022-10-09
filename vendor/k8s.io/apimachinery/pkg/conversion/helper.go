cb0ead509777d14887d9aaa3e9be060696eae506
platoapp <92391267+platoapp@users.noreply.github.com>
2022-10-09 06:09:01 +0000

    Fix last commit

--- vendor/k8s.io/apimachinery/pkg/conversion/helper.go
+++ vendor/k8s.io/apimachinery/pkg/conversion/helper.go
@@ -22,4 +22,18 @@ import (
 )
 
 // EnforcePtr ensures that obj is a pointer of some sort. Returns a reflect.Value
-// of the dereferenced po
+// of the dereferenced pointer, ensuring that it is settable/addressable.
+// Returns an error if this is not possible.
+func EnforcePtr(obj interface{}) (reflect.Value, error) {
+	v := reflect.ValueOf(obj)
+	if v.Kind() != reflect.Ptr {
+		if v.Kind() == reflect.Invalid {
+			return reflect.Value{}, fmt.Errorf("expected pointer, but got invalid kind")
+		}
+		return reflect.Value{}, fmt.Errorf("expected pointer, but got %v type", v.Type())
+	}
+	if v.IsNil() {
+		return reflect.Value{}, fmt.Errorf("expected pointer, but got nil")
+	}
+	return v.Elem(), nil
+}
